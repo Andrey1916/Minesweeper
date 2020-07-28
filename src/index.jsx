5a12d599f12c9121d847bff7090cafa68ad284e8
Andrey Shvidkyy <36729272+Andrey1916@users.noreply.github.com>
2020-07-28 19:47:37 +0300

    Initial commit

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import App from "./App.jsx";
+
+let rootEl = document.querySelector("#root");
+
+ReactDOM.render(<App />, rootEl);
